27fb0f800ea392b8392ad3531cc4ed56ec2cde30
Nery <dred22@outlook.com.br>
2025-12-04 18:57:47 -0300

    retirar botão login

--- frontend/zenith-house/src/pages/Login.tsx
+++ frontend/zenith-house/src/pages/Login.tsx
@@ -86,17 +86,7 @@ const Login = () => {
             {isLoading ? "Logging in..." : "Log In"}
           </button>
         </form>
-
-        {/* Link para Register */}
-        <p className="mt-6 text-center text-sm text-muted-foreground">
-          Don’t have an account?{" "}
-          <button
-            onClick={() => navigate("/register")}
-            className="text-primary hover:underline"
-          >
-            Sign up
-          </button>
-        </p>
+        
       </div>
     </div>
   );
